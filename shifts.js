ed87fec9ba1212bcf80181447b298e6498cee16e
renmaya <whatfu49@gmail.com>
2026-09-21 15:16:16 +0800

    Add weekend differential to calculatePay - amaya.annegwyn

--- shifts.js
+++ shifts.js
@@ -2,14 +2,18 @@ function isValidShift(hours) {
   return hours > 0 && hours <= 24;
 }
 
-function calculatePay(hours, rate) {
+function calculatePay(hours, rate, isWeekend) {
   let pay;
   if (hours > 8) {
     pay = 8 * rate + (hours - 8) * rate * 1.5;
   } else {
     pay = hours * rate;
   }
+  if (isWeekend) {
+    pay *= 1.1;
+  }
   return Math.round(pay);
 }
 
 module.exports = { isValidShift, calculatePay };
+
